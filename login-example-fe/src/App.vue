dca190ed68f43db9770c20aa35f9ee63a4a9477b
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2022-03-28 12:23:37 +0200

    First commit

---
+++ login-example-fe/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <h1>Welcome to the Login Example</h1>
+</div>
+  <img alt="Vue logo" src="./assets/LGL_logo.jpg">
+  <StartpageComp msg="Go to our login page"/>
+</template>
+
+<script>
+import StartpageComp from './components/StartpageComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    StartpageComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
