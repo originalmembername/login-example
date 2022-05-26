02e3b59027a63e0d1708a89c8e3618e587596cc1
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2022-05-26 20:41:31 +0200

    MockServer is now started in seperate function, TODO: Include handler for login

--- login-example-fe/src/components/Auth/authServer.js
+++ login-example-fe/src/components/Auth/authServer.js
@@ -1,4 +1,20 @@
-import { setupServer } from 'msw/node'
-import { authHandlers } from './authHandlers.js'
+import { rest } from "msw"
+import { setupWorker } from "msw"
 
-export const authServer = setupServer(...authHandlers)
+//export const authServer = setupServer(...authHandlers)
+
+//start the auth Server
+export function startMockServer() {
+    let worker = setupWorker(
+        // Provide request handlers
+        rest.get('/user/:userId', (req, res, ctx) => {
+            return res(
+                ctx.json({
+                    firstName: 'John',
+                    lastName: 'Maverick',
+                }),
+            )
+        }),
+    )
+    worker.start()
+}
--- login-example-fe/src/main.js
+++ login-example-fe/src/main.js
@@ -1,28 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import authcomp from './components/Auth/authcomp.js'
-import { setupWorker, rest } from 'msw'
+import { startMockServer } from './components/Auth/authServer'
 
 // Start the Mock Service Worker
-const worker = setupWorker(
-  // Provide request handlers
-  rest.get('/user/:userId', (req, res, ctx) => {
-    return res(
-      ctx.json({
-        firstName: 'John',
-        lastName: 'Maverick',
-      }),
-    )
-  }),
-)
-worker.start()
-
+startMockServer()
 
 //create app
 const app = createApp(App)
-//add authComp to app
-app.config.globalProperties.$authComp = authcomp
 //Mount app
 app.use(router).mount('#app');
 
