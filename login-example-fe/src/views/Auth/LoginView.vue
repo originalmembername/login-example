572f123be87a5f9573d2f314aef5f473573536fc
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2022-06-02 14:20:26 +0200

    Form validation for register form works

--- login-example-fe/src/views/Auth/LoginView.vue
+++ login-example-fe/src/views/Auth/LoginView.vue
@@ -58,48 +58,43 @@ export default {
             //Get username and password
             let user = this.input.username
             let pwd = this.input.password
-            if (user != "" && pwd != "") {
-
-                //send login request to server
-                axios.post('/login/auth', {
-                    params: {
-                        'user': user,
-                        'password': pwd
-                    }
+            //send login request to server
+            axios.post('/login/auth', {
+                params: {
+                    'user': user,
+                    'password': pwd
+                }
+            })
+                .then(response => {
+                    //login has been accepted
+                    let token = response.data.token
+                    console.log("Login accepted with token: " + token);
+                    //Set authenticated to "true" and store token
+                    authComp.isAuthenticated = true
+                    authComp.token = token
+                    //tell isAuthenticated to App component, so header can be adapted
+                    this.$emit("authenticated");
+                    //forward to restricted member page                      
+                    this.$router.push("/member");
                 })
-                    .then(response => {
-                        //login has been accepted
-                        let token = response.data.token
-                        console.log("Login accepted with token: " + token);
-                        //Set authenticated to "true" and store token
-                        authComp.isAuthenticated = true
-                        authComp.token = token
-                        //tell isAuthenticated to App component, so header can be adapted
-                        this.$emit("authenticated");
-                        //forward to restricted member page                      
-                        this.$router.push("/member");
-                    })
-                    .catch(error => {
-                        let status = error.response.status
-                        console.log("Error status: " + status)
-                        if (status == 401) {
-                            //Wrong password
-                            console.log("Wrong password")
-                            this.resetInput()
-                            return
-                        }
-                        if (status == 404) {
-                            //user doesn't exist
-                            console.log("User doesn't exist")
-                            this.resetInput()
-                            return
-                        }
-                        //if other error status, this shouldn't happen
-                        throw error
-                    })
-            } else {
-                console.log("A username and password must be present");
-            }
+                .catch(error => {
+                    let status = error.response.status
+                    console.log("Error status: " + status)
+                    if (status == 401) {
+                        //Wrong password
+                        console.log("Wrong password")
+                        this.resetInput()
+                        return
+                    }
+                    if (status == 404) {
+                        //user doesn't exist
+                        console.log("User doesn't exist")
+                        this.resetInput()
+                        return
+                    }
+                    //if other error status, this shouldn't happen
+                    throw error
+                })
         },
         async resetInput() {
             console.log("Resetting login input fields")
--- login-example-fe/src/views/Auth/RegisterView.vue
+++ login-example-fe/src/views/Auth/RegisterView.vue
@@ -1,18 +1,34 @@
 <template>
     <div id="register" class="container">
         <h1>Register</h1>
-        <div class="row"><input type="text" name="username" v-model="input.username" placeholder="Username" /></div>
-        <div class="row"><input type="password" name="password" v-model="input.password" placeholder="Password" /></div>
+        <div class="row"><input type="text" name="username" v-model="input.username" placeholder="Username" />
+            <div v-if="v$.input.username.$error" class="alert alert-warning" role="alert">
+                Username cannot be empty
+            </div>
+        </div>
+        <div class="row"><input type="password" name="password" v-model="input.password" placeholder="Password" />
+            <div v-if="v$.input.password.$error" class="alert alert-warning" role="alert">
+                Password cannot be empty
+            </div>
+        </div>
         <div class="row"><input type="password" name="password2" v-model="input.password2"
-                placeholder="Repeat Password" /></div>
+                placeholder="Repeat Password" />
+            <div v-if="v$.input.password2.$error" class="alert alert-warning" role="alert">
+                Both passwords must be identical
+            </div>
+        </div>
         <button type="button" v-on:click="register()">Register</button>
     </div>
 </template>
 
 <script>
+import useValidate from "@vuelidate/core";
+import { required, sameAs } from "@vuelidate/validators";
+
 export default {
     data() {
         return {
+            v$: useValidate(),
             input: {
                 username: "",
                 password: "",
@@ -20,9 +36,23 @@ export default {
             }
         }
     },
+    validations() {
+        return {
+            input: {
+                username: { required },
+                password: { required },
+                password2: { required, sameAsPassword: sameAs(this.input.password) }
+            }
+        }
+    },
     methods: {
         async register() {
-            console.log("Trying to register new user: " + this.username)
+            //validate form, cancel if there are errors
+            this.v$.$validate()
+            if (this.v$.$error) {                
+                return
+            }
+            console.log("For validation successful for new user: " + this.username)
         }
     }
 }
@@ -38,6 +68,7 @@ export default {
     margin-top: 200px;
     padding: 20px;
 }
+
 button {
     margin: 5px;
 }
