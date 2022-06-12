8b99e636fa60b72232b488ec18a6cfcca107e66e
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2022-06-12 15:54:01 +0200

    Reset changes in BE user model, try again

--- loginExampleBackend/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from tokenize import String
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class CustomUser(AbstractUser):
-    city = models.CharField(max_length=200, blank=True)
-    
