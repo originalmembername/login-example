98c4c510caee2b2f8349afb9b44212a2cfc2d901
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2022-06-13 17:33:44 +0200

    Added separate model UserInfo, added city + connection to User

--- loginExampleBackend/accounts/models.py
+++ loginExampleBackend/accounts/models.py
@@ -1,10 +1,8 @@
-""" from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-from django.contrib.auth.models import AbstractUser, User
-
-
-# Create your models here.
-class CustomUser(AbstractUser):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customUser')
-    city = models.CharField(max_length=100, default="")
- """
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=50, blank=True)
+    def __str__(self):
+        return self.user.get_username() + ", city: " + self.city
