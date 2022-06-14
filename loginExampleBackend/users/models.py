1c5b2a56dd7f2cddc1fb0cfe9b4cab2b13471b3f
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2022-06-14 09:52:51 +0200

    Created user creation form including user's city

--- loginExampleBackend/users/models.py
+++ loginExampleBackend/users/models.py
@@ -1,6 +1,38 @@
+from datetime import timezone
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager, UserManager
 
 
 class User(AbstractUser):
-    pass
+    city = models.CharField(max_length=50, null=True)
+
+    def _str_(self):
+        return self.username + " from " + self.city
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, city=None):
+      
+        now = timezone.now()
+        
+        if not username:
+            raise ValueError('The given username must be set')
+        email = UserManager.normalize_email(email)
+        user = self.model(username=username, email=email,city=city,
+                          is_staff=False, is_active=True, is_superuser=False,
+                          last_login=now, date_joined=now)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None,city=None):
+        now = timezone.now()
+        if not username:
+            raise ValueError('The given username must be set')
+        email = UserManager.normalize_email(email)
+        user = self.model(username=username, email=email,city=city,
+                          is_staff=False, is_active=True, is_superuser=False,
+                          last_login=now, date_joined=now)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
