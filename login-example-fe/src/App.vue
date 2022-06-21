fe9d7946eff766f407afbc4c0e38d3f158329395
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2022-06-21 15:19:41 +0200

    Implemented custom variables for Bootstrap in _custom_variables.scss

--- login-example-fe/src/App.vue
+++ login-example-fe/src/App.vue
@@ -41,7 +41,9 @@ export default {
 </script>
 
 <style lang="scss">
-@import "./styles/variables";
+//set global style variables, override bootstrap defaults
+@import "./styles/custom_variables"; 
+@import "bootstrap/scss/bootstrap.scss";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
