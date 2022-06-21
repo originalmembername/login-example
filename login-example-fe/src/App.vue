3cc9ec0d05984359c439f02a05881a8298e630e2
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2022-06-21 13:53:23 +0200

    Added global style variables from _variables.scss via import in App.vue

--- login-example-fe/src/App.vue
+++ login-example-fe/src/App.vue
@@ -40,7 +40,9 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "./styles/variables";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
